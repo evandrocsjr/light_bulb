4be5d62441a469937e2f3b0a911a02b423fd77aa
Evandro C <evandrocunhajunior@gmail.com>
2021-07-17 16:18:03 -0300

    Att language

--- lampada.js
+++ lampada.js
@@ -2,21 +2,21 @@ const t_on = document.getElementById('t-on')
 const t_off = document.getElementById('t-off')
 const lamp = document.getElementById('lamp')
 
-function ligar(){
+function turnOn(){
     lamp.setAttribute('src', './img/lampAcesa.png')
 }
 
-function desligar(){
+function turnOff(){
     lamp.setAttribute('src', './img/lampNormal.png')
 }
 
-function quebrar(){
+function toBreak(){
     lamp.setAttribute('src', './img/lampQuebrada.png')
 }
 
-lamp.addEventListener('mouseover', ligar)
-lamp.addEventListener('mouseleave', desligar)
-lamp.addEventListener('dblclick', quebrar)
-t_off.addEventListener('click', desligar)
-t_on.addEventListener('click', ligar)
+lamp.addEventListener('mouseover', turnOn)
+lamp.addEventListener('mouseleave', turnOff)
+lamp.addEventListener('dblclick', toBreak)
+t_off.addEventListener('click', turnOff)
+t_on.addEventListener('click', turnOn)
 
